2c6f55898bc53ed14b653636fb8f40fea4ea0ae1
Jack W <jackweatherilt@outlook.com>
2023-06-04 21:43:35 +0100

    Initial commit from me.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,116 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useEffect } from "react"
+
+type FormData = {
+  dayStartTime: string
+  dayEndTime: string
+  currentTime: string
+}
+
+const timeString = z
+  .string()
+  .regex(
+    /^\d{1,2}(?::\d{2})?$/,
+    "Please enter a valid time in the form hh:mm or hh"
+  )
+
+const schema = z.object({
+  dayStartTime: timeString,
+  dayEndTime: timeString,
+  currentTime: timeString,
+})
 
 export default function Home() {
+  const {
+    register,
+    watch,
+    formState: { errors },
+    setValue,
+  } = useForm<FormData>({
+    defaultValues: {
+      dayStartTime: "09:00",
+      dayEndTime: "23:00",
+      currentTime: new Date().toLocaleTimeString("en-US", {
+        hour12: false,
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    },
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  })
+
+  useEffect(() => {
+    const storedStartTime = localStorage.getItem("dayStartTime")
+    const storedEndTime = localStorage.getItem("dayEndTime")
+
+    if (storedStartTime) {
+      setValue("dayStartTime", storedStartTime)
+    }
+
+    if (storedEndTime) {
+      setValue("dayEndTime", storedEndTime)
+    }
+  }, [setValue])
+
+  const handleStartTimeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const startTime = event.target.value
+    setValue("dayStartTime", startTime)
+    localStorage.setItem("dayStartTime", startTime)
+  }
+
+  const handleEndTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const endTime = event.target.value
+    setValue("dayEndTime", endTime)
+    localStorage.setItem("dayEndTime", endTime)
+  }
+
+  let [currentMins, endMins, startMins] = watch([
+    "currentTime",
+    "dayEndTime",
+    "dayStartTime",
+  ]).map((item) => {
+    return item.split(":").reduce((acc, v) => acc * 60 + Number(v), 0)
+  })
+
+  if (endMins < startMins) {
+    endMins += 24 * 60
+  }
+
+  const p = (currentMins - startMins) / (endMins - startMins)
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <div>
+      <label>
+        Day start time
+        <input {...register("dayStartTime")} onChange={handleStartTimeChange} />
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          {errors.dayStartTime && <span>{errors.dayStartTime.message}</span>}
         </div>
-      </div>
+      </label>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <label>
+        Day end time
+        <input {...register("dayEndTime")} onChange={handleEndTimeChange} />
+        <div>
+          {errors.dayEndTime && <span>{errors.dayEndTime.message}</span>}
+        </div>
+      </label>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <label>
+        Current time
+        <input {...register("currentTime")} />
+        <div>
+          {errors.currentTime && <span>{errors.currentTime.message}</span>}
+        </div>
+      </label>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <h1>{Math.round(p * 100)}%</h1>
+    </div>
   )
 }
